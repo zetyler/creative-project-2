e711e9b5559fc01eecb702aa2e7595adb68b183a
Zachary Tyler <zack.tyler.938@gmail.com>
2019-02-15 21:19:15 -0700

    Added buttons for sprites.

--- js/script.js
+++ js/script.js
@@ -19,11 +19,48 @@ document.querySelector('#searchButton').addEventListener('click', function(event
             let flexBoxes = document.querySelectorAll('.flex-item');
             flexBoxes[0].innerText = capitalizeFirstLetter(jsonResult.name);
             flexBoxes[1].innerText = '#' + jsonResult.id;
-            flexBoxes[2].innerHTML = '<img src="' + jsonResult.sprites.front_default + '">';
-            document.querySelector('.flex-container').style.backgroundColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 60%)';
+            flexBoxes[2].querySelector('#sprite').innerHTML = '<img src="' + jsonResult.sprites.front_default + '">';
+            document.querySelector('.flex-container').style.backgroundColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 50%)';
             document.querySelector('.flex-container').style.borderColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 100%)';
             for(let box of flexBoxes) {
                 box.style.borderColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 100%)';
             }
         }).catch((error) => { console.log("Invalid Pokémon."); });
+});
+
+function getImgName(shiny, front) {
+    return ((front) ? "front" : "back") + "_" + ((shiny) ? "shiny" : "default");
+}
+
+function setSprite(shiny, front) {
+    document.querySelector('#sprite').innerHTML = '<img src="' + jsonResult.sprites[getImgName(shiny, front)] + '">';
+}
+
+let shinyButton = document.querySelector('#shinyButton');
+let shiny = false;
+let front = true;
+shinyButton.addEventListener('click', function(event) {
+    if (jsonResult) {
+        shinyButton.value = (shinyButton.value === "Not Shiny") ? "Shiny" : "Not Shiny";
+        shiny = !shiny;
+        setSprite(shiny, front);
+    }
+});
+
+document.querySelector('#frontButton').addEventListener('click', function(event) {
+    if (jsonResult) {
+        if(!front) {
+            front = true;
+            setSprite(shiny, front);
+        }
+    }
+});
+
+document.querySelector('#backButton').addEventListener('click', function(event) {
+    if (jsonResult) {
+        if (front) {
+            front = false;
+            setSprite(shiny, front);
+        }
+    }
 });
