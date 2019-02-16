3571cbb8d2e2533086d80849740c87d6edfb4411
Zachary Tyler <zack.tyler.938@gmail.com>
2019-02-15 19:36:14 -0700

    Added flex items and filled out some.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let jsonResult;
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 document.querySelector('#searchButton').addEventListener('click', function(event) {
     event.preventDefault();
     const inputValue = document.querySelector('#pokemonInput').value.toLowerCase();
@@ -9,9 +15,15 @@ document.querySelector('#searchButton').addEventListener('click', function(event
         .then((response) => { return response.json(); })
         .then((myJson) => { 
             console.log(myJson);
-            let newText = "";
-            newText += JSON.stringify(myJson);
-            let responseDiv = document.querySelector('#searchResponse');
-            responseDiv.innerHTML = newText;
+            jsonResult = myJson;
+            let flexBoxes = document.querySelectorAll('.flex-item');
+            flexBoxes[0].innerText = capitalizeFirstLetter(jsonResult.name);
+            flexBoxes[1].innerText = '#' + jsonResult.id;
+            flexBoxes[2].innerHTML = '<img src="' + jsonResult.sprites.front_default + '">';
+            document.querySelector('.flex-container').style.backgroundColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 60%)';
+            document.querySelector('.flex-container').style.borderColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 100%)';
+            for(let box of flexBoxes) {
+                box.style.borderColor = 'hsla(var(--' + jsonResult.types[0].type.name + '), 100%)';
+            }
         }).catch((error) => { console.log("Invalid Pokémon."); });
 });
